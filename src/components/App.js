ebb51d22350ed5213392ce1cc8aa2e8a398373b6
Andreas Karpasitis <karpasitis@outlook.com>
2019-07-13 17:53:34 +0200

    chore: add search component to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
 import { withRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Header from './Header';
+import Search from './Search';
 import Article from './Article';
 import Footer from './Footer';
 
 import { data } from '../data/articles.json';
 
 const App = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [isSearch, setIsSearch] = useState(false);
     const [currentArticleIndex, setCurrentArticleIndex] = useState(0);
     const numberOfArticles = data.length;
 
@@ -14,9 +17,21 @@ const App = () => {
         setCurrentArticleIndex(Number(articleIndex));
     };
 
+    const handleInputChange = event => {
+        const { target } = event;
+        const { value } = target;
+        setSearchTerm(value);
+        if (value === '') {
+            setIsSearch(false)
+        } else {
+            setIsSearch(true)
+        }
+    }
+
     return (
         <React.Fragment>
             <Header />
+            <Search isSearch={isSearch} searchTerm={searchTerm} handleInputChange={handleInputChange} />
             <Switch>
                 <Redirect exact from="/" to="/article/0" />
                 <Route path="/article/:index" render={() => <Article updateArticleIndex={updateArticleIndex} />} />
