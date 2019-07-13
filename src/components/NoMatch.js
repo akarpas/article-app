df436072a05c3458c4e2cf78d8d6ec17b991c6dc
Andreas Karpasitis <karpasitis@outlook.com>
2019-07-13 20:13:08 +0200

    feat: No matching url component

---
+++ src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import style from './NoMatch.scss';
+
+const NoMatch = () => (
+    <div className={style.noMatchContainer}>This article does not exist</div>
+);
+
+export default NoMatch;
