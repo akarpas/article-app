4aab790e0425ebd88460bc5fc583259b82f60af0
Andreas Karpasitis <karpasitis@outlook.com>
2019-07-13 17:31:14 +0200

    feat: create search component

---
+++ src/components/Search.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SearchResults from './SearchResults';
+
+import style from './Search.scss';
+
+const Search = props => {
+    const { handleInputChange, searchTerm, isSearch } = props;
+
+    return (
+        <div className={style.searchWrapper}>
+            <input value={searchTerm} onChange={handleInputChange} />
+            {isSearch && <SearchResults/> }
+        </div>
+    )
+};
+
+export default Search;
+
+Search.propTypes = {
+    handleInputChange: PropTypes.func,
+    searchTerm: PropTypes.string,
+    isSearch: PropTypes.bool
+};
---
+++ src/components/SearchResults.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// import PropTypes from 'prop-types';
+import style from './SearchResults.scss';
+
+const SearchResults = () => {
+    return (
+        <div className={style.resultsContainer}>Results</div>
+    );
+};
+
+export default SearchResults;
+
+SearchResults.propTypes = {};
