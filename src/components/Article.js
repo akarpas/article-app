ce9e82f6d97e10295d3175af741ae8c8c1f4ecf2
Andreas Karpasitis <karpasitis@outlook.com>
2019-07-13 17:00:53 +0200

    feat: add image url to body and render body and authors

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,40 @@
 import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
 
 import { data } from '../data/articles.json';
+
+const IMG_BASE_URL = '<img src="https://cdn2.audiencemedia.com';
 
 const Article = props => {
     const { match, updateArticleIndex } = props;
     const { params } = match;
     const { index } = params;
     const article = data[index];
+    const { authors } = article;
 
     useEffect(() => {
         updateArticleIndex(index);
     }, [params]);
 
-    return <div>article {article.title}</div>;
+    const parseBody = body => {
+        return body.replace(new RegExp('<img src="', 'g'), IMG_BASE_URL);
+    };
+
+    const body = parseBody(article.body);
+
+    return (
+        <div>
+            <h3>
+                {authors &&
+                    article.authors.map((author, i) => (
+                        <span key={author + i}>{author}</span>
+                    ))}
+            </h3>
+            <div>{ReactHtmlParser(body)}</div>
+        </div>
+    );
 };
 
 export default withRouter(Article);
